4f0b64638bfbdb4b2235e3c8d3bc9c4684d9545d
Juan Mendez <contact@juanmendez.info>
2016-12-06 20:02:52 -0600

    i thought i was not on master

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,9 +1,7 @@
  package info.juanmendez.md.materialdesigncompat;
 
- import android.support.design.widget.FloatingActionButton;
  import android.support.v7.app.AppCompatActivity;
  import android.support.v7.widget.Toolbar;
- import android.widget.Toast;
 
  import org.androidannotations.annotations.AfterViews;
  import org.androidannotations.annotations.EActivity;
@@ -15,15 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @ViewById
     Toolbar toolbar;
 
-    @ViewById
-    FloatingActionButton fab;
-
      @AfterViews
      void afterViews(){
          this.setSupportActionBar( toolbar );
-
-         fab.setOnClickListener(view -> {
-             Toast.makeText( MainActivity.this, "FAB clicked!!", Toast.LENGTH_LONG).show();
-         });
      }
 }
