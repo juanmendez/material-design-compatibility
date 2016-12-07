85bb8ae3fd5eecc0577177fc503c253ec9ed22f4
Juan Mendez <contact@juanmendez.info>
2016-12-07 10:56:57 -0600

    having now to select a menu item from navigationLayout

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,22 +1,37 @@
  package info.juanmendez.md.materialdesigncompat;
 
+ import android.support.annotation.NonNull;
+ import android.support.design.widget.NavigationView;
+ import android.support.v4.view.GravityCompat;
  import android.support.v4.widget.DrawerLayout;
  import android.support.v7.app.ActionBarDrawerToggle;
  import android.support.v7.app.AppCompatActivity;
  import android.support.v7.widget.Toolbar;
+ import android.view.MenuItem;
+ import android.widget.TextView;
 
  import org.androidannotations.annotations.AfterViews;
  import org.androidannotations.annotations.EActivity;
+ import org.androidannotations.annotations.InstanceState;
  import org.androidannotations.annotations.ViewById;
 
  @EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @ViewById
     Toolbar toolbar;
 
      @ViewById(R.id.drawer_layout)
      DrawerLayout drawerLayout;
+
+     @ViewById(R.id.txvSelectedText)
+     TextView selectedText;
+
+     @ViewById(R.id.nav_view)
+     NavigationView navigationView;
+
+     @InstanceState
+     String title = "";
 
      @AfterViews
      void afterViews(){
@@ -34,5 +49,38 @@ public class MainActivity extends AppCompatActivity {
 
          //we need to syncState, otherwise, the toggle button won't appear at the toolbar!
          drawerToggle.syncState();
+
+         navigationView.setNavigationItemSelectedListener(this);
+
+         selectedText.setText( title );
      }
-}
+
+
+     @Override
+     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+         String itemName = (String) item.getTitle();
+         selectedText.setText( itemName );
+         title = itemName;
+         hideDrawer();
+
+         return true;
+     }
+
+     @Override
+     public void onBackPressed(){
+         if( drawerLayout.isDrawerOpen( GravityCompat.START)){
+             hideDrawer();
+         }else{
+             super.onBackPressed();
+         }
+     }
+
+
+     private void hideDrawer(){
+         drawerLayout.closeDrawer( GravityCompat.START );
+     }
+
+     private void showDrawer(){
+         drawerLayout.openDrawer( GravityCompat.START );
+     }
+ }
