dfc9d1f4d03762f965427ddf57bee0020bf41e40
Juan Mendez <contact@juanmendez.info>
2016-12-06 19:28:14 -0600

    initial changes

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -2,9 +2,20 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.activity_main)
+ @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @ViewById
+    Toolbar toolbar;
+
+     @AfterViews
+     void afterViews(){
+         this.setSupportActionBar( toolbar );
+     }
 }
