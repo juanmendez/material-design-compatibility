cfe1b10dd1762a82fc9261a57eae44c1debd1e21
Juan Mendez <contact@juanmendez.info>
2016-12-06 19:48:23 -0600

    initial fab created with a toast on click

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,12 +1,13 @@
  package info.juanmendez.md.materialdesigncompat;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+ import android.support.design.widget.FloatingActionButton;
+ import android.support.v7.app.AppCompatActivity;
+ import android.support.v7.widget.Toolbar;
+ import android.widget.Toast;
 
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
+ import org.androidannotations.annotations.AfterViews;
+ import org.androidannotations.annotations.EActivity;
+ import org.androidannotations.annotations.ViewById;
 
  @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,15 @@ public class MainActivity extends AppCompatActivity {
     @ViewById
     Toolbar toolbar;
 
+    @ViewById
+    FloatingActionButton fab;
+
      @AfterViews
      void afterViews(){
          this.setSupportActionBar( toolbar );
+
+         fab.setOnClickListener(view -> {
+             Toast.makeText( MainActivity.this, "FAB clicked!!", Toast.LENGTH_LONG).show();
+         });
      }
 }
