416c375af2418207063e4017f946834589f6ef3e
Juan Mendez <contact@juanmendez.info>
2016-12-06 20:18:42 -0600

    there is a snackbar, but it's overlapping fab

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,11 +1,14 @@
  package info.juanmendez.md.materialdesigncompat;
 
  import android.support.design.widget.FloatingActionButton;
+ import android.support.design.widget.Snackbar;
  import android.support.v7.app.AppCompatActivity;
  import android.support.v7.widget.Toolbar;
+ import android.view.View;
  import android.widget.Toast;
 
  import org.androidannotations.annotations.AfterViews;
+ import org.androidannotations.annotations.Click;
  import org.androidannotations.annotations.EActivity;
  import org.androidannotations.annotations.ViewById;
 
@@ -25,5 +28,10 @@ public class MainActivity extends AppCompatActivity {
          fab.setOnClickListener(view -> {
              Toast.makeText( MainActivity.this, "FAB clicked!!", Toast.LENGTH_LONG).show();
          });
+     }
+
+     @Click(R.id.btn_snackbar)
+     void onBtnSnackbarClick(View view){
+         Snackbar.make( view, "Hello from SnackBar maker!", Snackbar.LENGTH_LONG ).show();
      }
 }
