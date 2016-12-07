c60cc2bb564fb7874e533fe91e1c6efc3fecfe42
Juan Mendez <contact@juanmendez.info>
2016-12-06 20:06:40 -0600

    now we are cooking!

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,10 +1,29 @@
  package info.juanmendez.md.materialdesigncompat;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+ import android.support.design.widget.FloatingActionButton;
+ import android.support.v7.app.AppCompatActivity;
+ import android.support.v7.widget.Toolbar;
+ import android.widget.Toast;
 
-import org.androidannotations.annotations.EActivity;
+ import org.androidannotations.annotations.AfterViews;
+ import org.androidannotations.annotations.EActivity;
+ import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.activity_main)
+ @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @ViewById
+    Toolbar toolbar;
+
+    @ViewById
+    FloatingActionButton fab;
+
+     @AfterViews
+     void afterViews(){
+         this.setSupportActionBar( toolbar );
+
+         fab.setOnClickListener(view -> {
+             Toast.makeText( MainActivity.this, "FAB clicked!!", Toast.LENGTH_LONG).show();
+         });
+     }
 }
