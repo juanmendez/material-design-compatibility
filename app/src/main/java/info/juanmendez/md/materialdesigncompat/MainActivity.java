798a50806ea50928bb4ade364317ccf23d762656
Juan Mendez <contact@juanmendez.info>
2016-12-07 08:55:43 -0600

    created customized snackbar

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,10 +1,12 @@
  package info.juanmendez.md.materialdesigncompat;
 
+ import android.graphics.Color;
  import android.support.design.widget.FloatingActionButton;
  import android.support.design.widget.Snackbar;
  import android.support.v7.app.AppCompatActivity;
  import android.support.v7.widget.Toolbar;
  import android.view.View;
+ import android.widget.TextView;
  import android.widget.Toast;
 
  import org.androidannotations.annotations.AfterViews;
@@ -46,9 +48,37 @@ public class MainActivity extends AppCompatActivity {
          Snackbar snackbarWithOption = Snackbar.make( v, "Hello from SnackBar maker!", Snackbar.LENGTH_LONG );
          snackbarWithOption.show(); //this can appear here or right below.
 
-         //this automatically adds RETRY option, and handles click action.
+
          snackbarWithOption.setAction("RETRY", view -> {
             Snackbar.make( view, "File loaded!", Snackbar.LENGTH_LONG).show();
          });
+     }
+
+     @Click( R.id.btn_snackbar_color )
+     void onBtnSnackbarCustomized( View v ){
+         Snackbar snackbarCustomized = Snackbar.make( v, "Hello from SnackBar maker!", Snackbar.LENGTH_LONG );
+
+         snackbarCustomized.setAction("RETRY", view -> {
+             Snackbar.make( view, "File loaded!", Snackbar.LENGTH_LONG).show();
+         });
+
+         View snackbarLayout = snackbarCustomized.getView();
+         snackbarLayout.setBackgroundColor( Color.BLUE );
+
+         /**we modify color by simply accessing Snackbar defined views we want to customize
+          * mMessageView = (TextView) findViewById(R.id.snackbar_text);
+          * mActionView = (Button) findViewById(R.id.snackbar_action);
+          */
+
+         TextView txvMessage = (TextView) snackbarLayout.findViewById( android.support.design.R.id.snackbar_text);
+         txvMessage.setTextColor( Color.YELLOW);
+
+         /*this works, or else what's below
+         Button optionButton = (Button) snackbarLayout.findViewById(android.support.design.R.id.snackbar_action);
+         optionButton.setTextColor( Color.WHITE );*/
+
+         snackbarCustomized.setActionTextColor( Color.WHITE );
+
+         snackbarCustomized.show(); //this can appear here or right below.
      }
 }
