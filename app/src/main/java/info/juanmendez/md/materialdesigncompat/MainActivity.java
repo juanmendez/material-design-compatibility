c112386c8fcce765c4a83f9c1e14dc2e266c4f90
Juan Mendez <contact@juanmendez.info>
2016-12-07 10:09:57 -0600

    having toggle button at toolbar

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,5 +1,7 @@
  package info.juanmendez.md.materialdesigncompat;
 
+ import android.support.v4.widget.DrawerLayout;
+ import android.support.v7.app.ActionBarDrawerToggle;
  import android.support.v7.app.AppCompatActivity;
  import android.support.v7.widget.Toolbar;
 
@@ -13,8 +15,24 @@ public class MainActivity extends AppCompatActivity {
     @ViewById
     Toolbar toolbar;
 
+     @ViewById(R.id.drawer_layout)
+     DrawerLayout drawerLayout;
+
      @AfterViews
      void afterViews(){
          this.setSupportActionBar( toolbar );
+
+         //in our other demo Android-Material-Design, the sliding fragment is the one used
+         //to connect drawerFragment.setUpDrawer(drawerLayout, toolbar);
+         //I tested using drawerToggle, and everything was fine as well.
+
+         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                 R.string.drawer_open,
+                 R.string.drawer_close );
+
+         drawerLayout.addDrawerListener( drawerToggle);
+
+         //we need to syncState, otherwise, the toggle button won't appear at the toolbar!
+         drawerToggle.syncState();
      }
 }
