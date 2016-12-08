ea779bb70e9e3c5fe47bbb796eaa9525de5bc9d8
Juan Mendez <contact@juanmendez.info>
2016-12-08 13:48:07 -0600

    made statusbar transparent for navigationView

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,86 +1,87 @@
- package info.juanmendez.md.materialdesigncompat;
+package info.juanmendez.md.materialdesigncompat;
 
- import android.support.annotation.NonNull;
- import android.support.design.widget.NavigationView;
- import android.support.v4.view.GravityCompat;
- import android.support.v4.widget.DrawerLayout;
- import android.support.v7.app.ActionBarDrawerToggle;
- import android.support.v7.app.AppCompatActivity;
- import android.support.v7.widget.Toolbar;
- import android.view.MenuItem;
- import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
 
- import org.androidannotations.annotations.AfterViews;
- import org.androidannotations.annotations.EActivity;
- import org.androidannotations.annotations.InstanceState;
- import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.InstanceState;
+import org.androidannotations.annotations.ViewById;
 
- @EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @ViewById
     Toolbar toolbar;
 
-     @ViewById(R.id.drawer_layout)
-     DrawerLayout drawerLayout;
+    @ViewById(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
-     @ViewById(R.id.txvSelectedText)
-     TextView selectedText;
+    @ViewById(R.id.txvSelectedText)
+    TextView selectedText;
 
-     @ViewById(R.id.nav_view)
-     NavigationView navigationView;
+    @ViewById(R.id.nav_view)
+    NavigationView navigationView;
 
-     @InstanceState
-     String title = "";
+    @InstanceState
+    String retainedTitle = "";
 
-     @AfterViews
-     void afterViews(){
-         this.setSupportActionBar( toolbar );
+    @AfterViews
+    void afterViews(){
+        this.setSupportActionBar( toolbar );
 
-         //in our other demo Android-Material-Design, the sliding fragment is the one used
-         //to connect drawerFragment.setUpDrawer(drawerLayout, toolbar);
-         //I tested using drawerToggle, and everything was fine as well.
+        //in our other demo Android-Material-Design, the sliding fragment is the one used
+        //to connect drawerFragment.setUpDrawer(drawerLayout, toolbar);
+        //I tested using drawerToggle, and everything was fine as well.
 
-         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                 R.string.drawer_open,
-                 R.string.drawer_close );
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close );
 
-         drawerLayout.addDrawerListener( drawerToggle);
+        drawerLayout.addDrawerListener( drawerToggle);
 
-         //we need to syncState, otherwise, the toggle button won't appear at the toolbar!
-         drawerToggle.syncState();
+        //we need to syncState, otherwise, the toggle button won't appear at the toolbar!
+        drawerToggle.syncState();
 
-         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
 
-         selectedText.setText( title );
-     }
-
-
-     @Override
-     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-         String itemName = (String) item.getTitle();
-         selectedText.setText( itemName );
-         title = itemName;
-         hideDrawer();
-
-         return true;
-     }
-
-     @Override
-     public void onBackPressed(){
-         if( drawerLayout.isDrawerOpen( GravityCompat.START)){
-             hideDrawer();
-         }else{
-             super.onBackPressed();
-         }
-     }
+        selectedText.setText(retainedTitle);
+    }
 
 
-     private void hideDrawer(){
-         drawerLayout.closeDrawer( GravityCompat.START );
-     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        String itemName = (String) item.getTitle();
+        selectedText.setText( itemName );
+        retainedTitle = itemName;
+        hideDrawer();
 
-     private void showDrawer(){
-         drawerLayout.openDrawer( GravityCompat.START );
-     }
- }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if( drawerLayout.isDrawerOpen( GravityCompat.START)){
+            hideDrawer();
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
+    private void hideDrawer(){
+        drawerLayout.closeDrawer( GravityCompat.START );
+    }
+
+    private void showDrawer(){
+        drawerLayout.openDrawer( GravityCompat.START );
+    }
+}
