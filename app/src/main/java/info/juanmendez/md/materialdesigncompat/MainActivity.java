a586f9df4c1b523b8a7a9d88e55646ea27c85251
Juan Mendez <contact@juanmendez.info>
2016-12-11 22:22:20 -0600

    now we can see the collapsingToolBar collapsing but keeping the toolbar

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,5 +1,6 @@
  package info.juanmendez.md.materialdesigncompat;
 
+ import android.support.design.widget.CollapsingToolbarLayout;
  import android.support.v7.app.AppCompatActivity;
  import android.support.v7.widget.Toolbar;
 
@@ -15,8 +16,14 @@ public class MainActivity extends AppCompatActivity {
     @ViewById
     Toolbar toolbar;
 
+     @ViewById
+     CollapsingToolbarLayout collapsingToolBar;
+
      @AfterViews
      void afterViews(){
          this.setSupportActionBar( toolbar );
+
+         //this overrides the title in toolbar
+         collapsingToolBar.setTitle( "Collapsing Toolbar");
      }
 }
