71ecfddd33006d6c0506d12c2cf7a583b0f63056
Juan Mendez <contact@juanmendez.info>
2016-12-12 00:09:31 -0600

    making sure everything is added and committed

--- app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
+++ app/src/main/java/info/juanmendez/md/materialdesigncompat/MainActivity.java
@@ -1,29 +1,29 @@
- package info.juanmendez.md.materialdesigncompat;
+package info.juanmendez.md.materialdesigncompat;
 
- import android.support.design.widget.CollapsingToolbarLayout;
- import android.support.v7.app.AppCompatActivity;
- import android.support.v7.widget.Toolbar;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
- import org.androidannotations.annotations.AfterViews;
- import org.androidannotations.annotations.EActivity;
- import org.androidannotations.annotations.OptionsMenu;
- import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
 
- @EActivity(R.layout.activity_main)
- @OptionsMenu(R.menu.menu)
+@EActivity(R.layout.activity_main)
+@OptionsMenu(R.menu.menu)
 public class MainActivity extends AppCompatActivity {
 
     @ViewById
     Toolbar toolbar;
 
-     @ViewById
-     CollapsingToolbarLayout collapsingToolBar;
+    @ViewById
+    CollapsingToolbarLayout collapsingToolBar;
 
-     @AfterViews
-     void afterViews(){
-         this.setSupportActionBar( toolbar );
+    @AfterViews
+    void afterViews() {
+        this.setSupportActionBar(toolbar);
 
-         //this overrides the title in toolbar
-         collapsingToolBar.setTitle( "Collapsing Toolbar");
-     }
+        //this overrides the title in toolbar
+        collapsingToolBar.setTitle("Collapsing Toolbar");
+    }
 }
